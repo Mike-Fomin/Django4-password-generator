7ddda48af215a8bb008f8fb3e5615ea908c51fb8
Mikhail Fomin <kuzzzmitch@mail.ru>
2023-03-13 11:53:54 +0530

    debug and footer_bg changes

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a@o7t7j6$k=0kb^%ih+m=t&c31i-%8k#ndm2oa#tvjflk9#+x4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
